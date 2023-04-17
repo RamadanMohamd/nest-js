6a0b827a19c5c81862cbb89e31b2ba7cdc09ea3e
Ramadan <ramadan@Ramadans-MBP.home>
2023-04-17 04:02:36 +0200

    edit user and test it

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,18 +1,30 @@
-import { Controller, Get, UseGuards, Req, Patch} from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
 import { User } from '@prisma/client';
-import { GetUser } from '../auth/decorator/get-user.decorator'
-import { JwtGurd } from  '../auth/gurd/jwt.gurd'
-@UseGuards(JwtGurd)
+import { GetUser } from '../auth/decorator';
+import { JwtGuard } from '../auth/gurd';
+import { EditUserDto } from './dto';
+import { UserService } from './user.service';
+
+@UseGuards(JwtGuard)
 @Controller('users')
 export class UserController {
-  
+  constructor(private userService: UserService) {}
   @Get('me')
-  getMe(@GetUser() user: User ) {
+  getMe(@GetUser() user: User) {
     return user;
   }
 
   @Patch()
-  editUser() {
-
+  editUser(
+    @GetUser('id') userId: number,
+    @Body() dto: EditUserDto,
+  ) {
+    return this.userService.editUser(userId, dto);
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,11 +3,16 @@ import {
   ValidationPipe,
 } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import { PrismaService } from '../src/prisma/prisma.service';
 import { AppModule } from '../src/app.module';
+import * as pactum from 'pactum';
+import { AuthDto } from '../src/auth/dto';
+import { EditUserDto } from '../src/user/dto';
 
 describe('App e2e', () => {
   let app: INestApplication;
-  beforeAll(async () => {
+  let prisma: PrismaService;
+  beforeAll( async () => {
     const moduleRef =
       await Test.createTestingModule({
         imports: [AppModule],
@@ -20,12 +25,144 @@ describe('App e2e', () => {
       }),
     );
 
-    await app.init()
+    await app.init();
+    await app.listen(3333);
+
+    prisma = app.get(PrismaService);
+    await prisma.cleanDb();
+    pactum.request.setBaseUrl(
+      'http://localhost:3333',
+    );
+
   });
 
-  afterAll(()=> {
-    app.close()
-  })
-  
-  it.todo('Pass test');
+  afterAll(() => {
+    app.close();
+  });
+
+  describe('Auth', () => {
+    const dto: AuthDto = {
+      email: 'ramii@gmail.com',
+      password: '12345',
+    };
+
+    describe('Signup', () => {
+      it('should throw error if no email provided',  () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      }, 7000 );
+
+      it('should throw error if no body provided',  () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .expectStatus(400);
+      }, 7000);
+
+      it('should throw error if no password provided',  () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      }, 7000);
+
+      it('should signup',  () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+      }, 7000);
+    });
+
+    describe('Signin',  () => {
+      it('should throw error if no email provided', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            password: dto.password,
+          })
+          .expectStatus(400);
+      }, 7000);
+
+      it('should throw error if no body provided',  () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .expectStatus(400);
+      }, 7000);
+
+      it('should throw error if no password provided',  () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({
+            email: dto.email,
+          })
+          .expectStatus(400);
+      },7000);
+
+      it('should signin',  () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody(dto)
+          .expectStatus(200)
+          .stores('userAt', 'access_token')
+      },7000);
+    });
+  });
+
+
+  describe('User', () => {
+
+    describe('Get me', () => {
+      it('should get current user',  () => {
+        return pactum
+        .spec()
+        .get('/users/me')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}'
+        })
+        .expectStatus(200)
+        .inspect() 
+      },7000)
+    });
+
+    describe('Edit User', () => {
+      it('should edit user',  () => {
+        const dto: EditUserDto = {
+          firstName: 'Ramii', 
+          email: 'Ramii@mohey.com'
+        }
+        return pactum
+        .spec()
+        .patch('/users')
+        .withHeaders({
+          Authorization: 'Bearer $S{userAt}'
+        })
+        .withBody(dto)
+        .expectStatus(200)
+        .inspect() 
+      },7000)
+    });
+    
+  });
+
+  describe('Bookmarks', () => {
+    it('Create bookmark', () => {});
+    it('Get bookmark', () => {});
+    it('Get bookmark by id ', () => {});
+    it('Edit bookmark', () => {});
+    it('Delete bookmark', () => {});
+  });
 });
